5e05cbccef428a5e102a76a7145210975957a43a
Raphael Da Silva <rapha.santi@gmail.com>
2025-05-27 21:44:03 +0100

    feat: conditionally render AddButton based on user type in Home component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,16 +2,18 @@
 import AddButton from "@/components/addButton";
 import CardList from "@/components/CardList";
 import Header from "@/components/Header";
+import { useUserType } from "@/hooks/useUserType";
 import { useState } from "react";
 
 export default function Home() {
   const [search, setSearch] = useState('');
+  const userType = useUserType();
 
   return (
     <main className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <Header search={search} setSearch={setSearch} />
       <CardList search={search} />
-      <AddButton />
+      {(userType === "Professor" || userType === "Administrador") && <AddButton />}
     </main>
   );
 }
